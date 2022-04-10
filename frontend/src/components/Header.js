e5f21f7d1fc4833eec8dcd0b7f342d3d08404e94
tapia1013 <manbearpig1013@gmail.com>
2022-04-10 15:08:52 -0700

    react router dom and bootstrap and replaced a tags with link tag and brought it browser router in app.js to let us route

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,17 +1,33 @@
 import React from 'react'
+// LinkContainer does the same as Link from react-router that lets us wrap around bootstrap components
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap'
+
+
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Shopping</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Shopping</Navbar.Brand>
+          </LinkContainer>
+
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <Nav.Link href='/cart'><i className='fas fa-shopping-cart'></i> Cart</Nav.Link>
-              <Nav.Link href='/login'><i className='fas fa-user'></i> Sign In</Nav.Link>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
