cea7bb609c1da9a1246bc4386e1c2381970c6409
tapia1013 <manbearpig1013@gmail.com>
2022-04-10 19:58:55 -0700

    backend product routes and error middlewares

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config()
 
@@ -10,19 +13,21 @@ connectDB()
 
 const app = express();
 
+
+
 app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find(p => p._id === req.params.id)
 
-  res.json(product)
-})
+// error 404 not found
+app.use(notFound)
+
+// error middleware
+app.use(errorHandler)
+
 
 const PORT = process.env.PORT || 5000
 
