46fea5e0bd1b2a8963575626ebc59aac9d6dc341
tapia1013 <manbearpig1013@gmail.com>
2022-04-12 12:36:35 -0700

    backend routes controller protectmiddleware for user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,7 +30,68 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid Email or Password')
   }
 
+})
 
+
+
+
+
+// @desc     Register a new user
+// @routes   POST /api/users
+// @access   Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExist = await User.findOne({ email })
+
+  if (userExist) {
+    res.status(400)
+    throw new Error('User already exists')
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if (user) {
+    res.status(201).res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id)
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+
+})
+
+
+
+
+// @desc     Get User Profile
+// @routes   GET /api/users/profile
+// @access   Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  // gets the user
+  const user = await User.findById(req.user._id)
+
+  // check for the user
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
 })
 
 
@@ -38,7 +99,8 @@ const authUser = asyncHandler(async (req, res) => {
 
 
 
-
 export {
-  authUser
+  authUser,
+  registerUser,
+  getUserProfile
 }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -28,10 +28,23 @@ const userSchema = mongoose.Schema({
 userSchema.methods.matchPassword = async function (enteredPassword) {
   // Built in method called compare that compares the plain textPW we get from the body to match the encrypted one in our database
   return await bcrypt.compare(enteredPassword, this.password)
-
 }
 
 
+// Before we save created user run this function
+userSchema.pre('save', async function (next) {
+  // only sent if pw field is sent or modified
+  if (!this.isModified('password')) {
+    next()
+  }
+
+  // encrypt pw for newly created user
+  const salt = await bcrypt.genSalt(10)
+
+  this.password = await bcrypt.hash(this.password, salt)
+})
+
 
 const User = mongoose.model('User', userSchema)
+
 export default User
