d838ef8120489f8e99fa0254e21d73d00c9e479a
tapia1013 <manbearpig1013@gmail.com>
2022-04-13 15:13:35 -0700

    shipping screen and checkout steps components

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,8 @@ import { Form, Button } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import FormContainer from '../components/FormContainer';
 import { saveShippingAddress } from '../actions/cartActions';
+import CheckoutSteps from '../components/CheckoutSteps';
+
 
 
 const ShippingScreen = ({ history }) => {
@@ -29,6 +31,8 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
+
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
