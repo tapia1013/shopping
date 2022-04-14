10ca1fe7c2c5c3e2780368a0531fe5ef2d73ddc7
tapia1013 <manbearpig1013@gmail.com>
2022-04-13 19:01:42 -0700

    created getOrderById and orderController

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -45,4 +45,27 @@ const addOrderItems = asyncHandler(async (req, res) => {
 
 
 
-export { addOrderItems }
+
+
+
+
+// @desc     Get order by ID
+// @routes   GET /api/orders/:id
+// @access   Private
+const getOrderById = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id).populate('user', 'name email')
+
+  // check if order exists
+  if (order) {
+    res.json(order)
+  } else {
+    req.status(404)
+    throw new Error('Order not found')
+  }
+
+})
+
+
+
+
+export { addOrderItems, getOrderById }
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,12 +1,15 @@
 import express from 'express';
 const router = express.Router();
 import {
-  addOrderItems
+  addOrderItems,
+  getOrderById
 } from '../controllers/orderController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 
 router.route('/').post(protect, addOrderItems)
+// :/id has to be at the bottom or else it wont find
+router.route('/:id').get(protect, getOrderById)
 
 
 export default router
